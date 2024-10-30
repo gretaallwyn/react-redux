0fbc99c0939c71247cfcd279a884a2aeee8ccef2
gretaallwyn <68838318+gretaallwyn@users.noreply.github.com>
2024-10-30 20:08:20 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { moviesList } from './actions';
+import { bindActionCreators } from 'redux';
+
+class App extends Component {
+
+  componentDidMount(){
+   // this.props.dispatch(moviesList())
+   this.props.moviesList();
+  }
+
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        {
+          this.props.movies ?
+            this.props.movies.map((item)=>(
+              <div key={item.id}>
+                {item.name}
+              </div>
+            ))
+          :null
+        }
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  console.log(state)
+  return {
+    movies: state.movies
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({moviesList},dispatch)
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
